3a9b5575f9bf10f668e92391fc5933b8c38ef6f9
fatenzouaghi <faten_zouaghi@uqar.ca>
2025-08-30 15:00:29 -0400

    convert_pressure_to_elevation.m

--- src/convert_pressure_to_elevation.m
+++ src/convert_pressure_to_elevation.m
@@ -1,32 +1,21 @@
-function PP = convert_pressure_to_elevation(ff, h_mean, opts, PPp)
-% CONVERT_PRESSURE_TO_ELEVATION  Convert pressure spectrum to elevation spectrum
-% using linear wave theory with or without attenuation.
+function PP = convert_pressure_to_elevation(ff, depth_mean, opts, PPp)
+% CONVERT_PRESSURE_TO_ELEVATION
+% Convert pressure spectrum (PPp) to surface elevation spectrum (PP)
+% using linear wave theory pressure response. If opts.use_attenuation is
+% false, apply a simple (rho*g)^-2 scaling.
 %
-% Inputs:
-%   ff      : frequency vector
-%   h_mean  : mean depth approximation
-%   opts    : options structure containing parameters
-%   PPp     : pressure PSD (Power Spectral Density)
+%   TF = cosh(k*h) / [cosh(k*z_sensor)] / (rho*g)
+%   PP = PPp * TF^2
 %
-% Output:
-%   PP      : elevation PSD (Power Spectral Density)
+% where h = depth_mean, z_sensor = opts.hd (height above bed).
+rho = 1023; g = 9.81;
 
-% Physical constants
-rho = 1023;  % Density of seawater (kg/m^3)
-g = 9.81;    % Gravitational acceleration (m/s^2)
-
-% If attenuation is used, apply the transfer function
 if opts.use_attenuation
-    % Compute the wave number using linear wave theory
-    k = wavek(ff, h_mean, g);
-    
-    % Compute the transfer function
-    TF = cosh(k * h_mean) ./ cosh(k * opts.hd) / (rho * g);  % Transfer function
-    
-    % Apply the transfer function to the pressure PSD to get elevation PSD
-    PP = PPp .* (TF .^ 2);  % Elevation PSD with attenuation
+    k  = wavek(ff, depth_mean, g);                   % user-provided function
+    TF = cosh(k.*depth_mean) ./ cosh(k.*opts.hd) / (rho*g);
+    PP = PPp .* (TF.^2);
 else
-    % Without attenuation: simple conversion to elevation PSD
-    PP = PPp / (rho * g) ^ 2;
+    PP = PPp ./ (rho*g).^2;
 end
 end
+
