bdf5779beee3607aca83b80b67b426be7ac9bd85
fatenzouaghi <faten_zouaghi@uqar.ca>
2025-09-01 17:35:03 -0400

    Delete src/merge_opts.m

--- src/merge_opts.m
+++
@@ -1,9 +0,0 @@
-function out = merge_opts(defaults, user)
-% MERGE_OPTS  Struct overlay: user values override defaults.
-out = defaults;
-if isempty(user), return; end
-fn = fieldnames(user);
-for i = 1:numel(fn)
-    out.(fn{i}) = user.(fn{i});
-end
-end
