4071e252ea884af6a6dd79a812bca490a8102fdb
fatenzouaghi <faten_zouaghi@uqar.ca>
2025-09-01 17:34:42 -0400

    Delete src/convert_pressure_to_elevation.m

--- src/convert_pressure_to_elevation.m
+++
@@ -1,9 +0,0 @@
-function PP = convert_pressure_to_elevation(ff, depth_mean, opts, PPp)
-
-rho = 1023; 
-g   = 9.81;
-
-k  = wavek(ff, depth_mean, g);                         % wave number
-TF = cosh(k.*depth_mean) ./ cosh(k.*opts.hd) / (rho*g);% pressure→elevation 
-PP = PPp .* (TF.^2);                                   % elevation PSD [m^2/Hz]
-end
